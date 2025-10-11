1786c8abedaaa3e39377d2c029cd2e377e5acc93
Andrew Gene Brown <brown.andrewg@gmail.com>
2025-10-10 18:48:26 -0700

    test: wbt_source: basic behaviors for vector and raster data sources

---
+++ tests/testthat/test-wbt_source.R
@@ -0,0 +1,66 @@
+test_that("wbt_source (raster) works", {
+
+    skip_if_not_installed("terra")
+
+    f <- sample_dem_data()
+    src <- wbt_source(f)
+
+    x <- attr(src, "wbt_dsn")
+
+    expect_true(grepl(".tif$", x))
+    expect_true(file.exists(x))
+
+    dem <- terra::rast(f)
+
+    src <- wbt_source(f)
+
+    x <- attr(src, "wbt_dsn")
+
+    expect_true(grepl(".tif$", x))
+    expect_true(file.exists(x))
+
+    tf <- tempfile(fileext = ".grd")
+    terra::writeRaster(dem, tf)
+
+    src <- wbt_source(tf)
+
+    x <- attr(src, "wbt_dsn")
+
+    expect_true(grepl(".tif$", x))
+    expect_true(file.exists(x))
+
+    unlink(tf)
+})
+
+test_that("wbt_source (vector) works", {
+
+    skip_if_not_installed("terra")
+
+    f <- sample_soils_data()
+    src <- wbt_source(f)
+
+    x <- attr(src, "wbt_dsn")
+
+    expect_true(grepl(".shp$", x))
+    expect_true(file.exists(x))
+
+    dem <- terra::vect(f)
+    src <- wbt_source(dem)
+
+    x <- attr(src, "wbt_dsn")
+
+    expect_true(grepl(".shp$", x))
+    expect_true(file.exists(x))
+
+    tf <- tempfile(fileext = ".geojson")
+    terra::writeVector(dem, tf)
+
+    src <- wbt_source(tf)
+
+    x <- attr(src, "wbt_dsn")
+
+    expect_true(grepl(".shp$", x))
+    expect_true(file.exists(x))
+
+    unlink(tf)
+})
